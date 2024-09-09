01a9061676a5398a9afe3a2cd73f68b2394a7ad0
Vishnu Mano <vishnu.mano@gmail.com>
2024-09-09 13:14:21 -0400

    team bio update 2

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -12,7 +12,7 @@ const Team = () => {
           <p className='md:px-5 text-white'>
             Vishnu is a co-founder at Paperboy. 
             He is currently studying Computer Science, Mathematics, and City Planning at Georgia Tech. 
-            Prior to building Paperboy, Vishnu worked in ML research, transportation tech, and venture capital, 
+            Prior to building Paperboy, Vishnu worked in machine learning research, transportation tech, and venture capital, 
             gaining experience in development, management, investment, and research.
           </p>
           <a href="https://www.linkedin.com/in/vishnumano/"><button className='md:mx-5 bg-[#E8C547] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Connect</button></a>
@@ -28,7 +28,7 @@ const Team = () => {
             Krishna is a co-founder at Paperboy. 
             As a junior at City High School (in Grand Rapids, MI), Krishna hosts a current-events talk show 
             called The Take with Krishna Mano, which covers various social, political, and economic headlines 
-            from around the world, having amassed over 50,000 views on Youtube.
+            from around the world, having amassed thousands of views on Youtube.
           </p>
           <a href="https://www.youtube.com/@TheTakeWithKrishnaMano"><button className='md:mx-5 bg-[#E8C547] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Connect</button></a>
         </div>
